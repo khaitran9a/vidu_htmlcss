2fba13c396324aabe1df91a20a3069738a2a76f3
Tran Van Khai <tranvankhai17042000@gmail.com>
2022-09-26 20:10:46 +0700

    26/9/2022

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// active list
+var header = document.getElementById("nav-list");
+var btns = header.getElementsByClassName("navbar-link js-scroll-trigger");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
